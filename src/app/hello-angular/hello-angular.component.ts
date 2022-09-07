bc3954219b2a3a61a41228619c8eda432bcd205c
Santos Pierre <santospierre1305@gmail.com>
2022-09-07 15:17:53 +0200

    Exo1 + Demo Routing

---
+++ src/app/hello-angular/hello-angular.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hello-angular',
+  templateUrl: './hello-angular.component.html',
+  styleUrls: ['./hello-angular.component.scss']
+})
+export class HelloAngularComponent implements OnInit {
+  prenom = "Jane";
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
